e80e5b5e6b57d806cb23da175a2f133dd76e917b
JurajSefcik <juraj.sefcik@nordlicht-it.com>
2023-05-12 13:35:44 +0200

    hopefuly first try deployment is succesful and authorization is working!

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": "Application ran successfully - authentication microservice is working!"}
+    return {"data": "Application ran successfully - authentication microservice is working! Also, its deployed!"}
 
 
 @app.post("authenticate")
